d0a2d922dae320efec5b71e0daafbb2f735e4d3e
fufako <patryko.mikiciuk@gmail.com>
2022-09-16 13:37:55 +0200

    Add firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app"
+const firebaseConfig = {
+  apiKey: "AIzaSyDVvZQRneu-ULN1MTWfi1tdhDSjxgNLK8s",
+  authDomain: "findguy-bd3c9.firebaseapp.com",
+  projectId: "findguy-bd3c9",
+  storageBucket: "findguy-bd3c9.appspot.com",
+  messagingSenderId: "591282467879",
+  appId: "1:591282467879:web:a96c6c264be615de530a02",
+}
+
+const app = initializeApp(firebaseConfig)
