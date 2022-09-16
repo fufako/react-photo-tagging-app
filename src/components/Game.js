9423d51047339f455f3ef8817032322ac37b8beb
fufako <patryko.mikiciuk@gmail.com>
2022-09-16 16:17:45 +0200

    Add checkCoords func

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,22 +1,53 @@
 import { useState, useEffect } from "react"
 import { db } from "../firebase-config"
 import { collection, getDocs } from "firebase/firestore"
+import Map from "../map1.jpg"
 function Game() {
   const [maps, setMaps] = useState([])
   const mapsCollectionRef = collection(db, "maps")
 
+  const getCoords = (e) => {
+    let currentTargetRect = e.currentTarget.getBoundingClientRect()
+    const event_offsetX = e.pageX - currentTargetRect.left
+    const event_offsetY = e.pageY - currentTargetRect.top
+
+    console.log(event_offsetX)
+    console.log(event_offsetY)
+  }
   useEffect(() => {
     const getMap = async () => {
       const data = await getDocs(mapsCollectionRef)
-      setMaps(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+      setMaps(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+        }))
+      )
     }
+
     getMap()
-    console.log(maps)
   }, [])
+
+  const checkCoords = (xUser, yUser) => {
+    const xCoords = maps[0].xCoord
+    const yCoords = maps[0].yCoord
+    console.log(xCoords)
+    console.log(yCoords)
+  }
 
   return (
     <div className="game">
-      <img src={maps[0].url} alt="" />
+      <img
+        src={Map}
+        alt=""
+        onClick={(e) => {
+          getCoords(e)
+        }}
+      />
+      <button
+        onClick={() => {
+          checkCoords(100, 100)
+        }}
+      ></button>
     </div>
   )
 }
